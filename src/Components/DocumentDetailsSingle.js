224cf792f5dfd4dec2c3fabefd43a2b1a4aea139
Madhubala24 <madhu24321@gmail.com>
2023-06-19 16:10:05 +0530

    document pages updated

--- src/Components/DocumentDetailsSingle.js
+++ src/Components/DocumentDetailsSingle.js
@@ -1,11 +1,98 @@
 import { Button, Card, Col, Dropdown, Form, InputGroup, Row, Table } from "react-bootstrap";
 import Eye from '../asserts/images/eye-icon.svg'
 import SiteLogo from '../asserts/images/site-logo-xxl.svg'
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link,useParams,useLocation  } from "react-router-dom";
+import { useState,useEffect, useContext } from "react";
+import { fetchSigmadocdetails } from '../apifunction';
+import { DataContext } from "../App";
+   
+const DocumentDetailsSingle= (props)=>{
+    const location = useLocation();
+    //  const allData = location.state.allData;
+    // const id = useContext(DataContext);
+    const {sigmaId} = useParams();
+    const [documentDetails, setDocumentDetails] = useState(null);
+    // const location = useLocation();  
+    // const [sigmaId, setSigmaId] = useState(''); // State variable for sigmaId
 
-function DocumentDetailsSingle() {
+    // const { sigmaId } = useParams();
+
+
     const [search, setSearch] = useState(false);
+    const searchParams = new URLSearchParams(location.search);
+    console.log("all",searchParams);
+    const id = searchParams.get('id');
+   
+    console.log("id",id)
+
+    // useEffect(()=>{fetchSigmadocdetails()},[])
+    // const fetchSigmadocdetails=async()=>{
+    //     //console.log("inside dbcallalgo function")  
+    //     let req = [];
+    //     // if(location.state.allData==null)
+    //     // {
+    //     //     console.log("check")
+    //     // }
+    //     if(location.state  === null || location.state  === "" || location.state  === " " || location.state === undefined || location.state === ''){
+    //   console.log("dd")
+    //     }
+        
+    //     else{
+    //     let getalgo=location.state.allData.sigmaId
+    //      console.log("str",getalgo);
+    //       }        
+    // }      
+    
+
+    useEffect(() => {
+        fetchSigmadocdetails(id)
+          .then(response => {
+            console.log("res",response)
+            const [success, data] = response;
+            if (success) {
+              setDocumentDetails(data);
+              console.log("data", data);
+            } else {
+              console.error('Error fetching document details');
+            }
+          })
+          .catch(error => {
+            console.error('Error fetching document details:', error);
+          });
+      }, [id]);
+
+
+    // useEffect(()=>{fetchSigmadocdetails(location.state.allData.sigmaId)
+    // },[]);
+    //       const fetchSigmadocdetails=async()=>{
+    //           //console.log("inside dbcallalgo function")  
+    //           let req = [];
+    //           if(location.state  === null || location.state  === "" || location.state  === " " || location.state === undefined || location.state === ''){
+    //           }
+    //           else{
+    //             let data = location.state.allData.sigmaId
+    //       fetchSigmadocdetails()
+    //         .then(response => {
+    //           const [success, data] = response;
+    //           if (success) {
+    //             setDocumentDetails(data);
+    //             console.log("data",documentDetails)
+    //           } else {
+    //             console.error('Error fetching document details');
+    //           }
+    //         })
+    //         .catch(error => {
+    //           console.error('Error fetching document details:', error);
+    //         });
+    //       }
+    //     }
+      
+    
+    
+      // Rest of the component code...
+    
+    
+
     return ( 
         <div>
             <div className="mb-20">
@@ -36,16 +123,16 @@ function DocumentDetailsSingle() {
                             <Dropdown.Item href="#/action-3">jpeg</Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
-                    <Link to="/resource-persist-job" className="me-2 btn-outline-gray btn-outline-gray-black">
+                    {/* <Link to="/resource-persist-job" className="me-2 btn-outline-gray btn-outline-gray-black">
                         <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="d-block" viewBox="0 0 16 16">
                             <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
                         </svg>
-                    </Link>
-                    <Button variant="outline-gray" onClick={() => setSearch(!search)}>
+                    </Link> */}
+                    {/* <Button variant="outline-gray" onClick={() => setSearch(!search)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="d-block" viewBox="0 0 16 16">
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                         </svg>
-                    </Button>
+                    </Button> */}
                 </Col>
                 <Col md={6}>
                     {search && (
@@ -79,69 +166,71 @@ function DocumentDetailsSingle() {
                     <Col md={8}>
                         <Table hover responsive>
                             <thead>
-                                <tr>
+                  <tr>
+                
                                     <th>Document Name</th>
-                                    <th>Document1</th>
+                                    <td>{documentDetails?.name__v}</td>
+                                    
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <th>Token ID</th>
-                                    <td>1</td>
-                                </tr>
-                                <tr>
-                                    <th>Document ID</th>
-                                    <td>12345</td>
-                                </tr>
-                                <tr>
-                                    <th>Version ID</th>
-                                    <td>17244</td>
-                                </tr>
-                                <tr>
-                                    <th>Document Date</th>
-                                    <td>05-06-2023</td>
-                                </tr>
-                                <tr>
-                                    <th>File Modified Date</th>
-                                    <td>10-06-2023</td>
-                                </tr>
-                                <tr>
-                                    <th>File Created Date</th>
-                                    <td>04-06-2023</td>
-                                </tr>
-                                <tr>
-                                    <th>Document Creation Date</th>
-                                    <td>05-06-2023</td>
-                                </tr>
-                                <tr>
-                                    <th>NFT Creation Status</th>
-                                    <td>Approved</td>
-                                </tr>
-                                <tr>
-                                    <th>UUID</th>
-                                    <td>001</td>
-                                </tr>
-                                <tr>
-                                    <th>Createby</th>
-                                    <td>Sigma Admin</td>
-                                </tr>
-                                <tr>
-                                    <th>Region</th>
-                                    <td>US-East North Carolin</td>
-                                </tr>
-                                <tr>
-                                    <th>Type</th>
-                                    <td>Single</td>
-                                </tr>
-                                <tr>
-                                    <th>Title</th>
-                                    <td>SIGMA DOCS</td>
-                                </tr>
-                                <tr>
-                                    <th>Source Vault ID</th>
-                                    <td>1002</td>
-                                </tr>
-                            </tbody>
+  <tr>
+    <th>Token ID</th>
+    <td>{documentDetails?.id}</td>
+  </tr>
+  <tr>
+    <th>Document ID</th>
+    <td>{documentDetails?.jobId}</td>
+  </tr>
+  <tr>
+    <th>Version ID</th>
+    <td>{documentDetails?.version_id}</td>
+  </tr>
+  <tr>
+    <th>Document Date</th>
+    <td>{documentDetails?.createdDate}</td>
+  </tr>
+  <tr>
+    <th>File Modified Date</th>
+    <td>{documentDetails?.createdDate}</td>
+  </tr>
+  <tr>
+    <th>File Created Date</th>
+    <td>{documentDetails?.createdDate}</td>
+  </tr>
+  <tr>
+    <th>Document Creation Date</th>
+    <td>{documentDetails?.createdDate}</td>
+  </tr>
+  <tr>
+    <th>NFT Creation Status</th>
+    <td>{documentDetails?.version_id}</td>
+  </tr>
+  <tr>
+    <th>UUID</th>
+    <td>{documentDetails?.nftCreationStatus}</td>
+  </tr>
+  <tr>
+    <th>Createby</th>
+    <td>{documentDetails?.createdBy}</td>
+  </tr>
+  <tr>
+    <th>Region</th>
+    <td>US-East North Carolin</td>
+  </tr>
+  <tr>
+    <th>Type</th>
+    <td>Single</td>
+  </tr>
+  <tr>
+    <th>Title</th>
+    <td>{documentDetails?.name__v}</td>
+  </tr>
+  <tr>
+    <th>Source Vault ID</th>
+    <td>{documentDetails?.tenantId}</td>
+  </tr>
+</tbody>
                         </Table>
                     </Col>
                 </Row>
--- src/apifunction.js
+++ src/apifunction.js
@@ -22,4 +22,334 @@ export const OrgAdminmailcheckget = async (id) => {
   }
 };
 
+export const OrgAdminmailcheckget1 = async(email) =>{
+   
+  let key = "BvXlBA50Iw58XBSBZltS2H5P9IwS76f9hojA6aE5";
+    //Get method start
+    try{
+      let response2 = await fetch(`/platform/v1/userdetail/${email}`, 
+      {
+          headers: {
+              'x-api-key': `${key}`    
+            },
+        }
+        )
+      //console.log(response2);
+      // let response = await axios.request(options2);
+      // tentidresponse= await response.data;
+      // console.log("response",tentidresponse)
+        
+      const data2 = await response2.json();
+      console.log("Api inside", data2)
+      // return {data2};
+      return [true,data2];
+    }catch(err){
+      console.log("vercelerrro",err)
+      return [false,""];
+    }
+     
+}
 
+export const CreateOrganizationPost = async (name, organizationname) =>
+{       
+  let key = "BvXlBA50Iw58XBSBZltS2H5P9IwS76f9hojA6aE5";
+  // let userID = localStorage.getItem('UserID');
+  // let connectAddress = localStorage.getItem("walletAddress");
+  // let network = "AB";
+  
+    axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+    //console.log("done1",response.data);
+      // console.log("date",date);
+      const options2 = {
+        method: 'POST',
+        url: '/platform/v1/org',
+        headers: {
+          'x-api-key': `${key}`    
+        },
+        data: {
+            'name': `${name}`,
+            'createdBy': `${organizationname}`,
+        }
+      };
+      let tentidresponse ="";
+      try {
+     let response = await axios.request(options2);
+     tentidresponse= await response.data;
+     console.log("response",tentidresponse);
+      }catch(error){
+        console.error("done2",error);
+      }
+      // axios.request(options2).then(function (response2) {
+      // //  window.location.reload();
+      // }).catch(function (error) {
+      //     console.error("done2",error);
+      // });
+     
+      return tentidresponse;
+}
+export const CreateOrguserrolepost = async (emailid,name,role,tenentid) =>
+{       
+  let key = "BvXlBA50Iw58XBSBZltS2H5P9IwS76f9hojA6aE5";
+  // let userID = localStorage.getItem('UserID');
+  // let connectAddress = localStorage.getItem("walletAddress");
+  // let network = "AB";
+  
+    axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+    //console.log("done1",response.data);
+      // console.log("date",date);
+      const options2 = {
+        method: 'POST',
+        url: '/platform/v1/userdetail',
+        headers: {
+          'x-api-key': `${key}`    
+        },
+        data: {
+            'emailId': `${emailid}`,
+            'userName': `${name}`,
+            'password':"",
+            'roleType': `${role}`,
+            'method':"",
+            'tennantId':`${tenentid}`
+        }
+      };
+      
+      axios.request(options2).then(function (response2) {
+       console.log("response",response2);
+       return response2;
+      //  window.location.reload();
+      }).catch(function (error) {
+          console.error("done2",error);
+      });
+}
+export const Orgadminsignup = async (emailid,Pwd) =>
+{       
+  let key = "BvXlBA50Iw58XBSBZltS2H5P9IwS76f9hojA6aE5";
+  // let userID = localStorage.getItem('UserID');
+  // let connectAddress = localStorage.getItem("walletAddress");
+  // let network = "AB";
+  
+    axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+    //console.log("done1",response.data);
+      // console.log("date",date);
+      const options2 = {
+        method: 'PUT',
+        url: '/platform/v1/userdetail',
+        headers: {
+          'x-api-key': `${key}`    
+        },
+        data: {
+            'password': `${Pwd}`,
+            'method':"SIGNIN",
+            'emailId': `${emailid}`
+          
+          
+        }
+      };
+      let usercheck=true;
+      try {
+        let response = await axios.request(options2);
+        let statuscheck= await response.data;
+        console.log("response",statuscheck);
+         }catch(error){
+           console.error("done2",error);
+           usercheck=false;
+         }
+         return usercheck;
+
+
+      // await axios.request(options2).then(function (response2) {
+      //  console.log("response",response2);
+      //  return true;
+      // //  window.location.reload();
+      // }).catch(function (error) {
+      //     console.error("done2",error);
+      //     return false;
+      // });
+}
+
+export const Orguserlogincheck = async (emailid,pwd) =>
+{       
+  let key = "BvXlBA50Iw58XBSBZltS2H5P9IwS76f9hojA6aE5";
+  // let userID = localStorage.getItem('UserID');
+  // let connectAddress = localStorage.getItem("walletAddress");
+  // let network = "AB";
+  
+    axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+    //console.log("done1",response.data);
+      // console.log("date",date);
+      const options2 = {
+        method: 'POST',
+        url: '/platform/v1/userdetailsid',
+        headers: {
+          'x-api-key': `${key}`    
+        },
+        data: {
+            'emailId': `${emailid}`,
+            'password':`${pwd}`
+           
+        }
+      };
+
+     let userlogincheck ="";
+      try {
+     let response = await axios.request(options2);
+     userlogincheck= await response.data;
+     console.log("response",userlogincheck);
+      }catch(error){
+        console.error("done2",error);
+      }
+    
+     
+      return userlogincheck;
+    
+}
+export const Sessionloginpost = async (currentDateTime,logouttime,userlogstatus,tenentid,role,email) =>
+{       
+  let key = "BvXlBA50Iw58XBSBZltS2H5P9IwS76f9hojA6aE5";
+  // let userID = localStorage.getItem('UserID');
+  // let connectAddress = localStorage.getItem("walletAddress");
+  // let network = "AB";
+  
+    axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+    //console.log("done1",response.data);
+      // console.log("date",date);
+      const options2 = {
+        method: 'POST',
+        url: '/platform/v1/session',
+        headers: {
+          'x-api-key': `${key}`    
+        },
+        data: {
+            'loginTime': currentDateTime.toString(),
+            'logoutTime': "",
+            'activity':`${userlogstatus}`,
+            'tennatId': `${tenentid}`,
+            'roleType':`${role}`,
+            'mailId':`${email}`
+        }
+      };
+      
+      axios.request(options2).then(function (response2) {
+       console.log("response",response2);
+       return response2;
+      //  window.location.reload();
+      }).catch(function (error) {
+          console.error("done2",error);
+      });
+}
+export const Sessionstatusget= async(email) =>{
+   
+  let key = "BvXlBA50Iw58XBSBZltS2H5P9IwS76f9hojA6aE5";
+    //Get method start
+    try{
+      let response2 = await fetch(`/platform/v1/session/${email}`, 
+      {
+          headers: {
+              'x-api-key': `${key}`    
+            },
+        }
+        )
+      //console.log(response2);
+      // let response = await axios.request(options2);
+      // tentidresponse= await response.data;
+      // console.log("response",tentidresponse)
+        
+      const data2 = await response2.json();
+      console.log("Api inside", data2)
+      // return {data2};
+      return [true,data2];
+    }catch(err){
+      console.log("vercelerrro",err)
+      return [false,""];
+    }
+     
+}
+export const Sessionstatusupdate = async (logintime,logouttime,logstatus,email) =>
+{       
+  let key = "BvXlBA50Iw58XBSBZltS2H5P9IwS76f9hojA6aE5";
+  // let userID = localStorage.getItem('UserID');
+  // let connectAddress = localStorage.getItem("walletAddress");
+  // let profilepicofuserdefault = "https://gateway.pinata.cloud/ipfs/Qma6vhaA98FmVUMyMUJLvdxVsT2wJJWH5key2gtKr3jftZ?_gl=1*j61wws";
+  
+    axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+    //console.log("done1",response.data);
+      // console.log("date",date);
+      const options2 = {
+        method: 'PUT',
+        url: '/platform/v1/session',
+        headers: {
+          'x-api-key': `${key}`    
+        },
+        data: {
+          'loginTime':`${logintime}`,
+          'logoutTime':`${logouttime}`,
+          'activity':`${logstatus}`,
+          'mailId':`${email}`,
+          
+           
+           
+        }
+      };
+
+     let userlogincheck ="";
+      try {
+     let response = await axios.request(options2);
+     userlogincheck= await response.data;
+     console.log("response",userlogincheck);
+      }catch(error){
+        console.error("done2",error);
+      }
+    
+     
+      return userlogincheck;
+    
+}
+export const fetchSigmadocByTid = async (start, limit, tenantId) => {
+  const url = '/platform/v1/sigmadocbytid';
+  const key = 'BvXlBA50Iw58XBSBZltS2H5P9IwS76f9hojA6aE5';
+  
+  axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
+  const options = {
+    method: 'POST',
+    url,
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': key
+    },
+    data: {
+      start: start,
+      limit: limit,
+      tenantId: tenantId
+    }
+  };
+
+  try {
+    const response = await axios(options);
+    const sigmadocData = response.data;
+    console.log('Response:', sigmadocData);
+    return sigmadocData;
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
+};
+export const fetchSigmadocdetails = async (sigmaId) => {
+  console.log("id", sigmaId);
+  let key = "BvXlBA50Iw58XBSBZltS2H5P9IwS76f9hojA6aE5";
+  try {
+    const response3 = await fetch(`/platform/v1/sigmadoc/${sigmaId}`, {
+      headers: {
+        'x-api-key': `${key}`
+      },
+    });
+    console.log("response", response3);
+
+    const data = await response3.json();
+    console.log("Api inside", data);
+    return [true, data];
+  } catch (err) {
+    console.log("Error fetching document details:", err);
+    return [false, ""];
+  }
+};
