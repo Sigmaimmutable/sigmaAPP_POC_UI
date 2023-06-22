15dfa09a8abf2cb4da28c175a764cc7982a95466
Prabhakar-git-official <prabhakar4git@gamil.com>
2023-06-22 17:17:08 +0530

    commit after added profilepage

--- src/Components/Snippets/ProfileHeader.js
+++ src/Components/Snippets/ProfileHeader.js
@@ -1,9 +1,47 @@
 import { Button, Col, Dropdown, Row, Tab, Tabs } from "react-bootstrap";
 import Avatar from "../../asserts/images/avartar.png"
-import { Link } from "react-router-dom";
-import LogoutIcon from "../../asserts/images/logout-icon.svg"
+import { Link,useHistory,useNavigate,Redirect,Navigate} from 'react-router-dom';
 
+import LogoutIcon from "../../asserts/images/logout-icon.svg"
+import React, { useEffect,useState, useMemo } from 'react'
+import {userprofileget,Sessionstatusget,Sessionstatusupdate} from '../../apifunction';
 function ProfileHeader() {
+    const[getIProfile,setgetIProfile]=useState("");  
+    const [loginstatus, setLoginstatus] = useState("")
+    const [currentDateTime, setCurrentDateTime] = useState(new Date().toLocaleString());
+    const navigate = useNavigate()
+    const getprofiledetails = async() =>{
+        let [data,userprofiledetail]=await userprofileget(localStorage.getItem("UserID"));
+        setgetIProfile(userprofiledetail);
+        console.log("userdetail1",userprofiledetail,userprofiledetail.emailId);
+        console.log("userdetail11",getIProfile.emailId,getIProfile.firstName);
+       }
+       useEffect(()=>{getprofiledetails()})
+       const Logout = async () =>
+       {  
+           console.log("Logtime12",currentDateTime);
+           let email=localStorage.getItem('UserID')
+           console.log("emailid",email)
+           let [checklogin,loginstauscheck] = await  Sessionstatusget(email);
+          setLoginstatus(loginstauscheck);
+          console.log("logincheck",loginstatus);
+          let sessionlogin= await Sessionstatusupdate(" ","currentDateTime","Logout",email);
+          console.log("sessionstatus",sessionlogin);
+          localStorage.setItem("Login",false)
+          localStorage.removeItem('Login');
+          localStorage.setItem("UserID"," ");
+          localStorage.removeItem('UserID');
+          localStorage.removeItem('UserName');
+          if ( localStorage.getItem('rememberMe')=== true) {
+           localStorage.removeItem('rememberMe');
+         } else {
+           localStorage.removeItem('rememberMe');
+         }
+         navigate('/');
+          
+         
+          
+       }
     return ( 
         <header className="app-header">
             <div className="container-fluid">
@@ -112,22 +150,38 @@ function ProfileHeader() {
 
                         <Dropdown align={"end"} autoClose="outside">
                             <Dropdown.Toggle variant="ava" className="avatar p-0 border-0 d-flex align-items-center" id="dropdown-basic">
-                                <strong className="d-none d-md-block">Asha</strong>
-                                <img src={Avatar} className="shadow" alt="Avatar" />
+                                <strong className="d-none d-md-block">{localStorage.getItem("UserName")===""||localStorage.getItem("UserName")===null||localStorage.getItem("UserName")===undefined || !localStorage.getItem("UserName")? "UserName":localStorage.getItem("UserName")}</strong>
+                                {getIProfile.profilePic === null || getIProfile.profilePic === "" || getIProfile.profilePic === undefined ?(<>
+                                    <img src={Avatar} className="shadow" alt="Avatar" />
+                                
+                                </>):(<>
+                                
+                                    <img src={getIProfile.profilePic} className="shadow" alt="Avatar" />
+                                </>)}
+
+                               
                             </Dropdown.Toggle>
 
                             <Dropdown.Menu className="dropdown-avatar px-3">
                                 <div className="d-flex py-3 border-bottom">
-                                    <img src={Avatar} alt="Avatar" />
+                                    {/* <img src={Avatar} alt="Avatar" /> */}
+                                    {getIProfile.profilePic === null || getIProfile.profilePic === "" || getIProfile.profilePic === undefined ?(<>
+                                    <img src={Avatar} className="shadow" alt="Avatar" />
+                                
+                                </>):(<>
+                                
+                                    <img src={getIProfile.profilePic}  alt="Avatar" />
+                                </>)}
+
 
                                     <div className="d-flex flex-column justify-content-between">
-                                        <h6>Asha</h6>
-                                        <p>sigma.super.user@gmail.com</p>
+                                        <h6>{localStorage.getItem("UserName")===""||localStorage.getItem("UserName")===null||localStorage.getItem("UserName")===undefined || !localStorage.getItem("UserName")? "UserName":localStorage.getItem("UserName")}</h6>
+                                        <p>{localStorage.getItem("UserID")===""||localStorage.getItem("UserID")===null||localStorage.getItem("UserID")===undefined || !localStorage.getItem("UserID")? "Email":localStorage.getItem("UserID")}</p>
                                     </div>
                                 </div>
                                 <div className="py-2 d-flex align-items-center justify-content-between">
                                     <Link to="/account" className="btn-link">My Account</Link>
-                                    <Button variant="logout"><img src={LogoutIcon} alt="LogoutIcon" /> Sign Out</Button>
+                                    <Button variant="logout" onClick={()=>{Logout()}}><img src={LogoutIcon} alt="LogoutIcon" /> Sign Out</Button>
                                 </div>
                             </Dropdown.Menu>
                         </Dropdown>
