1b2001f1caee45df00339d7bcd62efcc69b202d0
puneet93 <puneet.softweb@gmail.com>
2023-06-24 13:03:34 +0530

    new upload

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -180,7 +180,7 @@ const [country, setCountry] = useState('');
                                         {Img === null || Img === "" || Img === undefined ?(
                                         <>                                        
                                         <input id="upload" type="file" hidden onChange = {captureFile}/>
-                                        <label htmlFor='upload' className='p-2'>
+                                        <label htmlFor='upload' className='p-2 label-upload'>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi mb-3 bi-upload" viewBox="0 0 16 16">
                                                 <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
                                                 <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/>
