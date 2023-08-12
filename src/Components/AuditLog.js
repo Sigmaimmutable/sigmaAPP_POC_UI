acf5dc46f207606b49074b94084d1521f5649d76
Parvatha-2428 <parvathasri@bosonlabs.net>
2023-08-12 19:19:04 +0530

    new

--- src/Components/AuditLog.js
+++ src/Components/AuditLog.js
@@ -1,19 +1,19 @@
 import { Button, Col, Dropdown, Form, InputGroup, Modal, Row, Table } from "react-bootstrap";
 import Question from '../asserts/images/question-icon.svg'
-import { Link,useNavigate } from "react-router-dom";
-import { useEffect, useState, useContext } from "react";
-import { Sessionstatusget1 } from "../apifunction";
+import { Link,useNavigate, } from "react-router-dom";
+import { useEffect, useState, useContext,React } from "react";
+import { Sessionstatusget1,uservisitrecords } from "../apifunction";
 import { ToastContainer, Toast, Zoom, Bounce, toast} from 'react-toastify';
 
  
 function Audit(props) {
-  const [reachedLastPage, setReachedLastPage] = useState(false);
     const [limit, setlimit] = useState(249);
     const [search, setSearch] = useState(false);
     const [show, setShow] = useState(false);
     const [showButton, setShowButton] = useState(false);
     const [memberlistTable, setmemberlistTable] = useState([]);
-    const [userManage, setUserManage] = useState([""]);
+    const [userManage, setUserManage] = useState([""]);  //setuservisit
+    const [uservisit1, setuservisit] = useState([""]);
     const [deleteEmail, setDeleteEmail] = useState();
     const [gotValue, setGotValue] = useState(false);
     const handleClose = () => setShow(false);
@@ -44,12 +44,6 @@ const Users =async(start)=>{
         let [check, data] = await Sessionstatusget1(start);
         if (check) {  
             setUserManage(data);
-             // Check if this is the last page of data
-             if (data.length === 0) {
-              setReachedLastPage(true);
-          } else {
-              setReachedLastPage(false);
-          }
       }
         // settenentid(tenentid.tenantId);
         console.log("ticket",data);
@@ -90,7 +84,21 @@ const ticketTableFetch = async () => {
         setstartvalue(start);
         await Users(start);
     }
+    const uservisit = async () => {
+      try{
+          let [check, data2] = await uservisitrecords();
+          setuservisit(data2);
+          console.log("tenetid",data2);
+        
+      }catch(err){
+          console.error(err);
+      }
+  }
+useEffect(()=>{
+  uservisit();
+}
 
+);
   
     return ( 
         // <Layout getThemeMode={() => undefined} roleType = {props.roleType} getIProfile = {props.getIProfile}>
@@ -132,7 +140,7 @@ const ticketTableFetch = async () => {
             </Modal>
            
             <div className="mb-20">
-            <Table hover responsive>
+            {/* <Table hover responsive>
   <thead>
     <tr>
       <th className="text-center">Sl no</th>
@@ -174,7 +182,74 @@ const ticketTableFetch = async () => {
           </tr>
         )}
   </tbody>
+</Table> */}
+
+
+<Table hover responsive>
+    <thead>
+        <tr>
+            <th className="text-center">Sl no</th>
+            <th className="text-center">Email Id</th>
+            <th className="text-center">Role Type</th>
+            <th className="text-center">Activity</th>
+            <th className="text-center">Login Time</th>
+            {/* <th className="text-center">Logout Time</th> */}
+        </tr>
+    </thead>
+    <tbody>
+        {searchQuery ? (
+            searchDetails.map((x) => (
+                <tr key={x.id}>
+                    <td className="text-center">{x.id}</td>
+                    <td className="text-center">{x.mailId}</td>
+                    <td className="text-center">{x.roleType}</td>
+                    <td className="text-center">
+                        {!uservisit1.find((visit) => visit.algoAddress === x.mailId) && x.activity}
+                        {uservisit1.find((visit) => visit.algoAddress === x.mailId) && (
+                            <div>
+                                Wallet Type: {uservisit1.find((visit) => visit.algoAddress === x.mailId).walletType}
+                            </div>
+                        )}
+                    </td>
+                    <td className="text-center">{x.loginTime}</td>
+                    {/* <td className="text-center">{x.logoutTime}</td> */}
+                </tr>
+            ))
+        ) : (
+            Array.isArray(userManage) &&
+            userManage.map((x) => (
+                <tr key={x.id}>
+                    <td className="text-center">{x.id}</td>
+                    <td className="text-center">{x.mailId}</td>
+                    <td className="text-center">{x.roleType}</td>
+                    <td className="text-center">
+                        {!uservisit1.find((visit) => visit.algoAddress === x.mailId) && x.activity}
+                        {uservisit1.find((visit) => visit.algoAddress === x.mailId) && (
+                            <div>
+                                 {uservisit1.find((visit) => visit.algoAddress === x.mailId).walletType}
+                            </div>
+                        )}
+                    </td>
+                    <td className="text-center">{x.loginTime}</td>
+                    {/* <td className="text-center">{x.logoutTime}</td> */}
+                </tr>
+            ))
+        )}
+        {!searchQuery && !Array.isArray(userManage) && (
+            <tr>
+                <td colSpan="6" className="text-center">
+                    No data available
+                </td>
+            </tr>
+        )}
+    </tbody>
 </Table>
+
+
+
+
+
+
 
                 <Row className="mt-4">
                 <Col md={8} className="d-flex justify-content-md-end justify-content-center">
@@ -188,13 +263,7 @@ const ticketTableFetch = async () => {
                             </li>
                             <li><Link className="active" onClick={()=>{pagination(startvalue+10)}}>{startvalue?(startvalue/10)+1:'1'}</Link></li>
                             <li>
-                            <Link
-    className={`next ${reachedLastPage ? 'disabled' : ''}`}onClick={() => {
-        if (!reachedLastPage) {
-            pagination(startvalue + 10);
-        }
-    }}
->
+                            <Link className="next" onClick={()=>{pagination(startvalue+10)}}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
                                         <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
                                     </svg>
--- src/apifunction.js
+++ src/apifunction.js
@@ -1789,3 +1789,59 @@ export const Jobstatusget= async() =>{
     }
      
 }
+export const uservisitrecord= async(id) =>{
+   
+  let key = "BvXlBA50Iw58XBSBZltS2H5P9IwS76f9hojA6aE5";
+    //Get method start
+    try{
+      let response2 = await fetch(`/platform/v1/uservisitrecord/${id}`, 
+      {
+          headers: {
+              'x-api-key': `${key}`    
+            },
+        }
+        )
+      //console.log(response2);
+      // let response = await axios.request(options2);
+      // tentidresponse= await response.data;
+      // console.log("response",tentidresponse)
+        
+      const data2 = await response2.json();
+      console.log("Api inside", data2)
+      // return {data2};
+      return [true,data2];
+    }catch(err){
+      console.log("vercelerrro",err)
+      return [false,""];
+    }
+     
+}
+
+
+export const uservisitrecords= async(id) =>{
+   
+  let key = "BvXlBA50Iw58XBSBZltS2H5P9IwS76f9hojA6aE5";
+    //Get method start
+    try{
+      let response2 = await fetch(`/platform/v1/uservisitrecord/`, 
+      {
+          headers: {
+              'x-api-key': `${key}`    
+            },
+        }
+        )
+      //console.log(response2);
+      // let response = await axios.request(options2);
+      // tentidresponse= await response.data;
+      // console.log("response",tentidresponse)
+        
+      const data2 = await response2.json();
+      console.log("Api inside", data2)
+      // return {data2};
+      return [true,data2];
+    }catch(err){
+      console.log("vercelerrro",err)
+      return [false,""];
+    }
+     
+}
